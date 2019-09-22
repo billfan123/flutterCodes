619612211f1b497a879c583c83bb63b9fd66402c
billfan123 <599010414@qq.com>
2019-09-22 21:56:48 +0800

    class36
    Signed-off-by: billfan123 <599010414@qq.com>

---
+++ lib/class3/class36.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:untitled/class3/utils/SwitchAndCheckBoxTestRoute.dart';
+
+class class36 extends State<SwitchAndCheckBoxTestRoute> {
+  bool _switchSelected=true; //维护单选开关状态
+  bool _checkboxSelected=true;//维护复选框状态
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Switch(
+          value: _switchSelected,//当前状态
+          onChanged:(value){
+            //重新构建页面
+            setState(() {
+              _switchSelected=value;
+            });
+          },
+        ),
+        Checkbox(
+          value: _checkboxSelected,
+          activeColor: Colors.red, //选中时的颜色
+          onChanged:(value){
+            setState(() {
+              _checkboxSelected=value;
+            });
+          } ,
+        )
+      ],
+    );
+  }
+}
---
+++ lib/class3/utils/SwitchAndCheckBoxTestRoute.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:untitled/class3/class36.dart';
+
+class SwitchAndCheckBoxTestRoute extends StatefulWidget {
+  @override
+  class36 createState() => new class36();
+}
